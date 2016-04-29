f545be1b4f3f4478aebae812068d714836026cb1
Toni Langley <chibia2@gmail.com>
2016-04-28 23:06:16 -0400

    able to display fake data for /anime

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+var db = require("./db/connection")
 
 app = express();
 
@@ -16,6 +17,12 @@ app.use("/assets", express.static("public"));
 app.get("/" , function(req, res){
   res.render("welcome");
 });
+
+app.get("/anime", function(req, res){
+  res.render("genre", {
+    anime: db.anime
+  })
+})
 
 app.listen(3009, function(){
   console.log("...andddd ACTION!");
