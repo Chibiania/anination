71f6d6c0e6f1d08d3aece24b5ac52c9b939e79ce
Toni Langley <chibia2@gmail.com>
2016-04-28 23:28:44 -0400

    installed/connected mongoose

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,18 @@
-var seedData = require("./seeds.json");
+var mongoose = require("mongoose");
 
+var AnimeSchema = new mongoose.Schema(
+  {
+    name: String,
+    genre: String,
+    description: String
+    // rank: integer
+  }
+);
+
+mongoose.model("Anime", AnimeSchema);
+mongoose.connect("mongodb//localhost/anination");
+
+var seedData = require("./seeds.json");
 module.exports = {
   anime: seedData
-}
+};
