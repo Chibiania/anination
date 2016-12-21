0c343e8ddfe3aafdcf2a6770019d3607c7a61a14
Toni Langley <chibia2@gmail.com>
2016-12-21 12:56:48 -0500

    working genre route

--- index.js
+++ index.js
@@ -9,108 +9,130 @@ var Anime = mongoose.model("Anime");
 
 app.set("port", process.env.PORT || 3009);
 app.set("view engine", "hbs");
-app.engine(".hbs", hbs ({
-  extname: ".hbs",
-  partialsDir: "views/",
-  layoutsDir: "views/",
-  defaultLayout: "layout"
+app.engine(".hbs", hbs({
+    extname: ".hbs",
+    partialsDir: "views/",
+    layoutsDir: "views/",
+    defaultLayout: "layout"
 }));
 
 app.use("/assets", express.static("public"));
 
-app.get("/" , function(req, res){
-  res.render("welcome");
+app.get("/", function(req, res) {
+    res.render("welcome");
 });
 
-app.get("/aboutme", function(req, res){
-  res.render("aboutme");
+app.get("/aboutme", function(req, res) {
+    res.render("aboutme");
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact");
+app.get("/contact", function(req, res) {
+    res.render("contact");
 });
 
 
-app.get("/anime", function(req, res){
-  Anime.find().then(function(anime){
-    res.render("index", {
-      anime: anime
+app.get("/anime", function(req, res) {
+    Anime.find().then(function(anime) {
+        res.render("index", {
+            anime: anime
+        });
     });
-  });
 });
 
-app.get("/anime/genre", function(req, res){
-  Genre.find().then(function(genre){
-    res.render("genre-index", {
-      genre: genre
+app.get("/anime/:name", function(req, res) {
+    Anime.findOne({
+        name: req.params.name
+    }).then(function(anime) {
+        res.render("anime-show", {
+            anime: anime
+        })
+    })
+});
+
+app.get("/genre", function(req, res) {
+    Genre.find().then(function(genre) {
+        res.render("genre-index", {
+            genre: genre
+        });
     });
-  });
 });
 
-app.get("/anime/action", function(req, res){
-  Genre.find().then(function(genre){
-    // console.log(genre);
-    res.render("genre-show", {
-      genre: genre
+app.get("/genre/:name", function(req, res) {
+    Genre.findOne({
+        name: req.params.name
+    }).then(function(gen) {
+        res.render("genre-show", {
+            gen: gen
+        });
     });
-  });
 });
 
-app.get("/anime/comedy", function(req, res){
-  Anime.find().then(function(anime){
-    res.render("comedy", {
-      anime: anime
-    });
-  });
-});
-
-app.get("/anime/horror", function(req, res){
-  Anime.find().then(function(anime){
-    res.render("horror", {
-      anime: anime
-    });
-  });
-});
-
-app.get("/anime/adventure", function(req, res){
-  Anime.find().then(function(anime){
-    res.render("adventure", {
-      anime: anime
-    });
-  });
-});
-
-
-app.get("/anime/kids", function(req, res){
-  Anime.find().then(function(anime){
-    res.render("kids", {
-      anime: anime
-    });
-  });
-});
-
-app.get("/anime/drama", function(req, res){
-  Genre.findOne().then(function(genre){
-    console.log(genre);
-    res.render("drama", {
-      genre: genre
-    });
-  });
-});
-
-app.get("/anime/:name", function(req, res){
-  Anime.findOne({name: req.params.name}).then(function(anime){
-    res.render("anime-show", {
-      anime: anime
-    });
-  });
-});
+// app.get("/anime/action", function(req, res) {
+//     Genre.find().then(function(genre) {
+//         // console.log(genre);
+//         res.render("genre-show", {
+//             genre: genre
+//         });
+//     });
+// });
+//
+// app.get("/anime/comedy", function(req, res) {
+//     Anime.find().then(function(anime) {
+//         res.render("comedy", {
+//             anime: anime
+//         });
+//     });
+// });
+//
+// app.get("/anime/horror", function(req, res) {
+//     Anime.find().then(function(anime) {
+//         res.render("horror", {
+//             anime: anime
+//         });
+//     });
+// });
+//
+// app.get("/anime/adventure", function(req, res) {
+//     Anime.find().then(function(anime) {
+//         res.render("adventure", {
+//             anime: anime
+//         });
+//     });
+// });
+//
+//
+// app.get("/anime/kids", function(req, res) {
+//     Anime.find().then(function(anime) {
+//         res.render("kids", {
+//             anime: anime
+//         });
+//     });
+// });
+//
+// app.get("/anime/drama", function(req, res) {
+//     Genre.findOne().then(function(genre) {
+//         console.log(genre);
+//         res.render("drama", {
+//             genre: genre
+//         });
+//     });
+// });
+//
+// app.get("/anime/:name", function(req, res) {
+//     Anime.findOne({
+//         name: req.params.name
+//     }).then(function(anime) {
+//         res.render("anime-show", {
+//             anime: anime
+//         });
+//     });
+// });
 
 
 // about me get
 
 // contact get
 
-app.listen(app.get("port"), function(){
-  console.log("...andddd ACTION!");
+app.listen(app.get("port"), function() {
+    console.log("...andddd ACTION!");
 });
