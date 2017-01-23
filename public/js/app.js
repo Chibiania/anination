4d3b4998561eed12031b9bf9be78fa34579c3c2d
Toni Langley <chibia2@gmail.com>
2017-01-23 13:51:38 -0500

    added angular routes

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,24 @@
             "ui.router",
             "ngResource"
         ])
-})
+        .config([
+            "$stateProvider",
+            RouterFunction
+        ])
+
+    function RouterFunction($stateProvider) {
+        $stateProvider
+            .state("welcome", {
+                url: "/",
+                templateUrl: "/assets/js/ng-views/welcome.html"
+            })
+            .state("aboutme", {
+                url: "/aboutme",
+                templateUrl: "/assets/js/ng-views/aboutme.html"
+            })
+            .state("contact", {
+                url: "/contact",
+                templateUrl: "/assets/js/ng-views/contact.html"
+            })
+    }
+})();
