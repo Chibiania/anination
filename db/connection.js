e236ddbdd39de18f9aa5b031aa55764540a3e798
Toni Langley <chibia2@gmail.com>
2017-01-23 13:06:51 -0500

    made minor changes

--- db/connection.js
+++ db/connection.js
@@ -1,31 +1,26 @@
 var mongoose = require("mongoose");
 
-var AnimeSchema = new mongoose.Schema(
-  {
+var AnimeSchema = new mongoose.Schema({
     name: String,
     img: String,
     description: String,
     watch_url: String
     // rank: Number
-  }
-);
+});
 
-var GenreSchema = new mongoose.Schema(
-  {
+var GenreSchema = new mongoose.Schema({
     name: String,
     img: String,
     anime: [AnimeSchema]
-  }
-);
+});
 
 mongoose.model("Genre", GenreSchema);
 mongoose.model("Anime", AnimeSchema);
 
-if(process.env.NODE_ENV == "production"){
-  mongoose.conect(process.env.MONGOLAB_URL);
-}
-else{
-  mongoose.connect("mongodb://localhost/anination");
+if (process.env.NODE_ENV == "production") {
+    mongoose.conect(process.env.MONGOLAB_URL);
+} else {
+    mongoose.connect("mongodb://localhost/anination");
 }
 
 module.exports = mongoose;
