8a41fd1d0308a1178fc73f77f79556cbc6c064b2
Toni Langley <chibia2@gmail.com>
2017-01-23 13:05:39 -0500

    added genre.anime loop

--- db/seed.js
+++ db/seed.js
@@ -6,17 +6,27 @@ var genre = require("./genre");
 var Genre = mongoose.model("Genre");
 var Anime = mongoose.model("Anime");
 
-Anime.remove().then(function(){
-  Anime.collection.insert(anime).then(function(){
-    Genre.remove().then(function(){
-      Genre.collection.insert(genre).then(function(){
-        // for(var i=0; i<anime.length; i++){
-        //   anime[i].genre.push(genre[i])
-        // }
-        console.log(anime);
-        console.log(genre);
-        process.exit();
-      });
+anime.forEach(function(ani) {
+    genre.forEach(function(gen) {
+        if (ani.genre == gen.name) {
+            gen.anime.push(ani)
+            Genre.update({
+                "name": gen.name
+            }, {
+                $push: {
+                    anime: ani
+                }
+            })
+        }
+    })
+})
+
+Anime.remove({}).then(function() {
+    Anime.collection.insert(anime).then(function() {
+        Genre.remove({}).then(function() {
+            Genre.collection.insert(genre).then(function() {
+                process.exit();
+            });
+        });
     });
-  });
 });
