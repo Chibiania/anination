0c13a5334c4201e2241105720b5938946e8ac1f3
Toni Langley <chibia2@gmail.com>
2017-01-23 13:11:01 -0500

    genre and anime routes render json

--- index.js
+++ index.js
@@ -31,107 +31,45 @@ app.get("/contact", function(req, res) {
 });
 
 
-app.get("/anime", function(req, res) {
+app.get("/api/anime", function(req, res) {
     Anime.find().then(function(anime) {
-        res.render("index", {
-            anime: anime
-        });
+        // res.render("index", {
+        //     anime: anime
+        // });
+        res.json(anime)
     });
 });
 
-app.get("/anime/:name", function(req, res) {
+app.get("/api/anime/:name", function(req, res) {
     Anime.findOne({
         name: req.params.name
     }).then(function(anime) {
-        res.render("anime-show", {
-            anime: anime
-        })
+        // res.render("anime-show", {
+        //     anime: anime
+        // })
+        res.json(anime)
     })
 });
 
-app.get("/genre", function(req, res) {
+app.get("/api/genre", function(req, res) {
     Genre.find().then(function(genre) {
-        res.render("genre-index", {
-            genre: genre
-        });
+        // res.render("genre-index", {
+        //     genre: genre
+        // });
+        res.json(genre)
     });
 });
 
-app.get("/genre/:name", function(req, res) {
+app.get("/api/genre/:name", function(req, res) {
     Genre.findOne({
         name: req.params.name
-    }).then(function(gen) {
-        res.render("genre-show", {
-            gen: gen
-        });
+    }).then(function(genre) {
+        // res.render("genre-show", {
+        //     genre: genre
+        // });
+        res.json(genre)
     });
 });
-
-// app.get("/anime/action", function(req, res) {
-//     Genre.find().then(function(genre) {
-//         // console.log(genre);
-//         res.render("genre-show", {
-//             genre: genre
-//         });
-//     });
-// });
-//
-// app.get("/anime/comedy", function(req, res) {
-//     Anime.find().then(function(anime) {
-//         res.render("comedy", {
-//             anime: anime
-//         });
-//     });
-// });
-//
-// app.get("/anime/horror", function(req, res) {
-//     Anime.find().then(function(anime) {
-//         res.render("horror", {
-//             anime: anime
-//         });
-//     });
-// });
-//
-// app.get("/anime/adventure", function(req, res) {
-//     Anime.find().then(function(anime) {
-//         res.render("adventure", {
-//             anime: anime
-//         });
-//     });
-// });
-//
-//
-// app.get("/anime/kids", function(req, res) {
-//     Anime.find().then(function(anime) {
-//         res.render("kids", {
-//             anime: anime
-//         });
-//     });
-// });
-//
-// app.get("/anime/drama", function(req, res) {
-//     Genre.findOne().then(function(genre) {
-//         console.log(genre);
-//         res.render("drama", {
-//             genre: genre
-//         });
-//     });
-// });
-//
-// app.get("/anime/:name", function(req, res) {
-//     Anime.findOne({
-//         name: req.params.name
-//     }).then(function(anime) {
-//         res.render("anime-show", {
-//             anime: anime
-//         });
-//     });
-// });
-
-
-// about me get
-
-// contact get
 
 app.listen(app.get("port"), function() {
     console.log("...andddd ACTION!");
